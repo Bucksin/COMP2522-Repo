43a1558a10e2e266cc842a51954f788d5a92a483
Bucksin <bucksin8@gmail.com>
2022-09-06 11:23:47 -0700

    first commit

---
+++ src/main/java/org/comp2522/Main.java
@@ -0,0 +1,7 @@
+package org.comp2522;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Buck and my fav animal are armadillos.");
+    }
+}
